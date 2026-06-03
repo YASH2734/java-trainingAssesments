d36acd0b639fc7a82963ca6e6ef590ec27b0196a
Yash Parit <yashparit165@gmail.com>
2026-06-03 13:47:12 +0530

    Java Day-1 Day-2

---
+++ src/day2/methodOverriding.java
@@ -0,0 +1,37 @@
+package day2;
+
+
+class Car{                   //parent class
+	void ApplyBrake(){
+		
+		System.out.println("Apply brake");
+	}
+}
+
+class Tata extends Car{
+	
+	void ApplyBrake(){
+		
+		System.out.println(" Tata Apply brake");
+	}
+	
+}
+
+public class methodOverriding {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Car car = new Tata();
+		car.ApplyBrake();
+		
+		Tata tata= new Tata();
+		tata.ApplyBrake();
+		
+		Car car1 = new Car();
+		car1.ApplyBrake();
+
+	}
+
+}
+
