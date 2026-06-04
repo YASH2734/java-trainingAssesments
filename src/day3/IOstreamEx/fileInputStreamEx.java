df1149ceeb14288e203bcca492d012efab7a6c3a
Yash Parit <yashparit165@gmail.com>
2026-06-04 16:17:16 +0530

    Done

---
+++ src/day3/IOstreamEx/fileInputStreamEx.java
@@ -0,0 +1,23 @@
+package day3.IOstreamEx;
+import java.io.FileInputStream;  // Import FileInputStream
+import java.io.IOException;      // Import IOException
+
+public class fileInputStreamEx {
+  public static void main(String[] args) {
+    // try-with-resources: FileInputStream will be closed automatically
+    try (FileInputStream input = new FileInputStream("filename.txt")) {
+
+      int i;  // variable to store each byte that is read
+ 
+             // Read one byte at a time until end of file (-1 means "no more data")
+      while ((i = input.read()) != -1) {
+            // Convert the byte to a character and print it to the console
+        System.out.print((char) i);
+      }
+
+    } catch (IOException e) {
+
+      System.out.println("Error reading file.");
+    }
+  }
+}
